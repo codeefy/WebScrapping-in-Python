b1b0c75bfdae0e54d4d324ec01c3cf5b1f7ec41c
codeefy <cashleyrohit01@gmail.com>
2026-04-25 15:34:52 +0530

    first commit

---
+++ Retrieving_html.py
@@ -0,0 +1,3 @@
+import requests
+
+url="https://www.flipkart.com/apple-iphone-17-pro-silver-256-gb/p/itm106f475c264c7?pid=MOBHFN6YPFSDYRTY&lid=LSTMOBHFN6YPFSDYRTYSCL89I&hl_lid=&marketplace=FLIPKART&fm=eyJ3dHAiOiJwbXVfdjIiLCJwcnB0IjoiaHAiLCJtaWQiOiJjb250aW51dW0vaHAifQ%3D%3D&pageUID=1777110860582"
